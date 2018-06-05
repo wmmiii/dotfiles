64d77edfe21a1a0ba006171b6f24ffbfbae68917
Will Martin <will@sketchup.com>
2018-06-05 14:27:37 -0600

    Fixed prompt escape characters.

--- configuration/bashrc.sh
+++ configuration/bashrc.sh
@@ -11,4 +11,4 @@ export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWDIRTYSTATE=1
 
 export PS1='\u \W$(__git_ps1 " (%s)")\$ '
-export PROMPT_COMMAND='__git_ps1 "$TXT_DIM$TXT_MAGENTA\u$TXT_RESET \W" "\\\$ "'
+export PROMPT_COMMAND='__git_ps1 "\[$TXT_DIM$TXT_MAGENTA\]\u\[$TXT_RESET\] \W" "\\\$ "'
