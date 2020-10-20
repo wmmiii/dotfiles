1f5b7d33d3a675f6b8ac9c5357c5b5b2637a2b6a
will@engflow.com <will@engflow.com>
2020-10-19 20:26:53 -0600

    Fixed incorrect user home
    Removed bash_profile
    
    Added vim to list of installed packages

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+
 source "${BASH_SOURCE%/*}/utils/text.sh"
 
 # Force root.
@@ -15,17 +17,15 @@ source utils/install.sh
 # Update packages.
 update_package_manager
 
+install_package vim;
 install_package htop;
 install_package tmux;
 
 # Add source to profile. 
-append_to_file $HOME/.bashrc "source $HOME/dotfiles/configuration/bashrc.sh"
-
-# Map bash_profile to bashrc.
-append_to_file $HOME/.bash_profile "source $HOME/dotfiles/configuration/bash_profile.sh"
+append_to_file $USER_HOME/.bashrc "source $USER_HOME/dotfiles/configuration/bashrc.sh"
 
 # Add source to vimrc.
-append_to_file $HOME/.vimrc "source $HOME/dotfiles/configuration/vimrc"
+append_to_file $USER_HOME/.vimrc "source $USER_HOME/dotfiles/configuration/vimrc"
 
 # Add source to tmux.
-append_to_file $HOME/.tmux.conf "source-file $HOME/dotfiles/configuration/tmux"
+append_to_file $USER_HOME/.tmux.conf "source-file $USER_HOME/dotfiles/configuration/tmux"
